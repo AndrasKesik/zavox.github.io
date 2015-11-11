aa54ff7c17c0c7d85b71be6e32531e5a368d1e80
zavox <andras.kesik@gmail.com>
2015-11-11 07:20:12 +0100

    working js on click

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 var main = function() {
-    $('#proba').fadeOut(3000);
-    $('#proba1').fadeOut(2000);
-    $('#proba2').fadeOut(1000);
-    $('#proba').fadeIn(1000);
-    $('#proba1').fadeIn(2000);
-    $('#proba2').fadeIn(3000);
+    $('#button1').click(function(){
+        $('#proba').fadeOut(3000);
+        $('#proba1').fadeOut(2000);
+        $('#proba2').fadeOut(1000);
+
+    });
+
+    $('#button2').click(function(){
+        $('#proba').fadeIn(1000);
+        $('#proba1').fadeIn(2000);
+        $('#proba2').fadeIn(3000);
+
+
+    });
+
 };
 
 $(document).ready(main);
